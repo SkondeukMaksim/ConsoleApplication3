fbb65e5c29cc0bfa16a6f4e456507dacc4042fe5
Користувач <@DESKTOP-04VSHRP>
2025-03-18 16:04:50 +0100

    Добавьте файлы проекта.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double R1, R2, R3, R0; 
+
+    /*cout << "Enter R1: "; 
+    cin >> R1; 
+    cout << "Enter R2: "; 
+    cin >> R2; 
+    cout << "Enter R3: "; 
+    cin >> R3; 
+
+    R0 = 1 / (1 / R1 + 1 / R2 + 1 / R3); 
+    cout << "R0 = " << R0 << endl; 
+
+    return 0;*/ 
+
+    /*double L, R, S;
+
+    cout << "Enter L: "; 
+    cin >> L; 
+
+    R = L / (2 * 3.14); 
+    S = 3.14 * pow(R, 2);
+    cout << "S = " << S << endl; 
+    return 0; */
+
+    /*double v, t, a, S; 
+
+    cout << "Enter v: "; 
+    cin >> v; 
+    cout << "Enter t: "; 
+    cin >> t; 
+    cout << "Enter a: "; 
+    cin >> a; 
+
+    S = v * t + (a * pow(t, 2)) / 2; 
+    cout << "S = " << S << endl; 
+
+    return 0; */
+}
